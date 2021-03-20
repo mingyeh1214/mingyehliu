e428d481a2ba61da86cdec367b54a5e6b2472c79
mingyeh1214 <mingyeh1214@gmail.com>
2021-03-21 00:31:33 +0800

    update

---
+++ apps/HOME.py
@@ -0,0 +1,21 @@
+import os
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+# app = dash.Dash(
+#   __name__, 
+#   external_stylesheets=[dbc.themes.BOOTSTRAP],
+#   meta_tags=[
+#     {'name': 'viewport',
+#     'content': 'width=device-width, initial-scale=1.0'}
+#   ]
+# )
+
+layout = html.Div([
+ "Home Page"
+  ])
+  
+# if __name__ == '__main__':
+#   app.run_server(debug = False)
